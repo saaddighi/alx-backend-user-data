1176cbb198db986904361e805451e90617cac48f
saaddighi <saaddighi20@gmail.com>
2024-12-25 11:09:58 +0000

    ok

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, 
-    separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
     """fucntion that returns the log message obfuscated
     """
     for i in fields:
